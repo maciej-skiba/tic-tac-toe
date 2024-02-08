4e45cd6d823f92af4b893d8e8768b827663faddc
Maciej Skiba <maciej.skiba035@gmail.com>
2024-02-08 01:10:15 +0100

    fixed add listener square

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 let toMove = "X";
+
+const toMoveImg = document.getElementById("to-move-img");
 const squares = document.getElementsByClassName("square");
 
+document.addEventListener('DOMContentLoaded', Prepare);
+
+function Prepare() {
+    
+    console.log("Window loaded.");
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('click', Move, true);
+    }
+}
+
 function Move() {
+    console.log(toMove + " Moved");
     if (toMove == "X") toMove = "O";
     else toMove = "X";
-    alert("toMove = ", toMove);
+    toMoveImg.src = "js/" + toMove + ".png";
 }
 
 Array.from(squares).forEach(square => {
-    square.addEventListener('click', Move());
 });
